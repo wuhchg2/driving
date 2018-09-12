335677cde2e87026a1d615bb1ba24f41b81595c1
HC WU <sunyata.plus@gmail.com>
2018-09-12 13:01:23 +0800

    add us

--- driving.py
+++ driving.py
@@ -1,9 +1,14 @@
-country = input('你是哪國人: ')
+country = input('你的國家 tw, us : ')
 age = input('輸入你的年齡: ')
 age = int(age)
-if country == '台灣':
+if country == 'tw':
     if age >= 18:
         print('你可以考駕照')
     else:
         print('你還不能考駕照')
-    
+elif country == 'us':
+    if age >= 16:
+        print('你可以考駕照')
+    else:
+        print('你還不能考駕照')
+   
